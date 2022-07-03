22d86cbb9d7919bf95f31adc2ff4b545be367d67
Andrew <40237958+reset54@users.noreply.github.com>
2022-07-03 09:40:54 +0300

    Update install_git_with_params.sh

--- install_git_with_params.sh
+++ install_git_with_params.sh
@@ -1,15 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 sudo apt update &&  sudo apt-get update && sudo apt upgrade
-
 # git install
 sudo apt install git
-
-# add Name to git
-git config --global user.name "reset54"
-
+# Variables:
+$GIT_PERSONAL_USERNAME=$1
+$GIT_PERSONAL_EMAIL=$2
+# add Name to git $USER = reset54, whoami
+git config --global user.name "$GIT_PERSONAL_USERNAME"
 # add Email to git
-git config --global user.email "zerg547@gmail.com"
-
+git config --global user.email "$GIT_PERSONAL_EMAIL"
 # check our installation
 git --version
+# show contents in file ~/.gitconfig
+git config --list --show-origin
 
+echo $GIT_PERSONAL_USERNAME
