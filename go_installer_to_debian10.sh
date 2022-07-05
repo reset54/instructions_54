d5cdd67819cc727562efd44f739cbc05cfa37ef4
reset54 <reset54@yandex.ru>
2022-07-05 22:01:19 +0300

    Modify /usr/local/go and add move /go (from tar) to /usr/local

--- go_installer_to_debian10.sh
+++ go_installer_to_debian10.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 # sudo chmod +x go_installer_to_debian10.sh
 sudo apt update && sudo apt-get update
-sudo apt install curl
-
-$GO_TAR_GZ = go1.15.linux-amd64.tar.gz
-# curl -O link_download_go_tar.gz
-curl -O https://dl.google.com/go/$GO_TAR_GZ
+# create DIR and change directory to $HOME/go 
+mkdir $HOME/go 
+cd $HOME
+# wget link_download_go_tar.gz
+sudo apt install wget -y
+GO_TAR_GZ="go1.18.3.linux-arm64.tar.gz"
+wget https://go.dev/dl/$GO_TAR_GZ
 # Compare the hash in the output with the checksum value on the Go download page. 
-# If they match, then the download can be considered valid.
-sha256sum $GO_TAR_GZ
 # After downloading Go and verifying the integrity of the file, you can begin the installation process.
-tar xvf $GO_TAR_GZ
+sudo tar -xzvf $GO_TAR_GZ
+
+# Change the go directory to the user and group root and move it to /usr/local:
+sudo chown -R root:root ./go
+sudo mv go /usr/local
+
+# to the PATH environment variable by adding
+export GOROOT="/usr/local/go"
+export GOPATH="$HOME"
+export PATH="$PATH:/usr/local/go/bin"
+export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+# check
+echo "GOPATH = $GOPATH" 
+echo "GOROOT = $GOROOT"
+
+# delete archive
+rm -r go1.18.3.linux-arm64.tar.gz*
+
+# check result
+sha256sum $GO_TAR_GZ
+go version
